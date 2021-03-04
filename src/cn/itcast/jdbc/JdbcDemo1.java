23ab4ece6985afe47f3ebcff4ab3d9450ad35675
jacky <571034970@qq.com>
2021-03-04 09:32:11 +0800

    默认的

---
+++ src/cn/itcast/jdbc/JdbcDemo1.java
@@ -0,0 +1,12 @@
+package cn.itcast.jdbc;
+
+import java.sql.*;
+
+
+//导入包
+public class JdbcDemo1{
+    public static void main(String[] args) {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+    }
+}
